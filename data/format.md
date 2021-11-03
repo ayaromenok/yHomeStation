04aebda2f8219eb97986442086bfb7a2aceaf938
Andrey Yaromenok <ayaromenok@gmail.com>
2021-11-03 18:16:16 +0200

    #1 wind, temperature Feel as, Humidity added

--- data/format.md
+++ data/format.md
@@ -1,13 +1,13 @@
 (Hourly Weather)	"Kiev, Ukraine"
-(full time)	"As of 2:27 pm EET"
+(full time)	"As of 2:167 pm EET"
 (fulldate)	"Wednesday, November 3"
 (time)		"3 pm"
 (temp in F)	"54°"
 (clouds)	"Cloudy"/Partly Cloudy/Mostly Cloudy/Clear/Mostly Clear/Few Showers
 (chance of rain) "2%:
-(wind)		"WSW 5 mph"
+(wind)		"WSW 7 mph"
 
-(Feels Like)	"53°"
-(Wind)		"WSW 5 mph"
-(Humidity)	"79%"
+(Feels Like)	"52°"
+(Wind)		"WSW 7 mph"
+(Humidity)	"77%"
 (UV Index)	"0 of 10"
--- weatherData/yweatherparser.cpp
+++ weatherData/yweatherparser.cpp
@@ -42,7 +42,32 @@ YWeatherParser::processLine(QString* line){
         qint32 posFullTimeBegin = line->indexOf("As of");
         qint32 posFullTimeEnd = line->indexOf("EET",posFullTimeBegin);
         if ((posFullTimeBegin>0) & (posFullTimeEnd>posFullTimeBegin)){
-            qDebug()<< "Full Time" << line->sliced(posFullTimeBegin+6,(posFullTimeEnd-posFullTimeBegin-1-6)) << posFullTimeBegin << posFullTimeEnd;
-        }
+            qDebug()<< "Full Time" << line->sliced(posFullTimeBegin+6,(posFullTimeEnd-posFullTimeBegin-1-6))
+                    << posFullTimeBegin << posFullTimeEnd;
+            qint32 posTempEnd = line->indexOf("°",posFullTimeEnd);
+            qint32 posTempBegin = line->indexOf(">",posTempEnd-4); //F in range 0...999. \todo - negative range
+            if (posTempBegin>0){
+                qDebug()<< "Temp in F" << line->sliced(posTempBegin+1,(posTempEnd-posTempBegin-1))
+                        << posTempBegin << posTempEnd;
+                qint32 posWindEnd = line->indexOf("mph",posTempEnd);
+                qint32 posWindBegin = line->indexOf(">",posWindEnd-15); //mph, some <!-- --> before
+                if (posTempBegin>0){
+                    qDebug()<< "Wind in mph" << line->sliced(posWindBegin+1,(posWindEnd-posWindBegin-2))
+                            << posWindBegin << posWindEnd;
+                    qint32 posTempFeelEnd = line->indexOf("°",posWindEnd);
+                    qint32 posTempFeelBegin = line->indexOf(">",posTempFeelEnd-4); //F in range 0...999. \todo - negative range
+                    if (posTempFeelBegin>0){
+                        qDebug()<< "Temp feel in F" << line->sliced(posTempFeelBegin+1,(posTempFeelEnd-posTempFeelBegin-1))
+                                << posTempFeelBegin << posTempFeelEnd;
+                        qint32 posHumidityEnd = line->indexOf("%",posTempFeelEnd);
+                        qint32 posHumidityBegin = line->indexOf(">",posHumidityEnd-3); //% in range 0..99
+                        if (posHumidityBegin>0){
+                            qDebug()<< "Humidity in %" << line->sliced(posHumidityBegin+1,(posHumidityEnd-posHumidityBegin-1))
+                                    << posHumidityBegin << posHumidityEnd;
+                        }//Humidity
+                    }//Temperature/Feel as
+                }//Wind
+            }//Temperature
+        }//if posFullTime
     }//if data from Kiev
 }
